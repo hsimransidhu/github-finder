940355cb552b7dca92131241e94e29bdf0174962
hsimransidhu <simusidhu2004@gmail.com>
2024-06-14 03:00:24 -0500

    Deploy React app to GitHub Pages

--- src/config.js
+++
@@ -1,6 +0,0 @@
- 
- export const token = process.env.REACT_APP_GITHUB_TOKEN;
-// GitHub API token for authentication
-export const options = {
-  headers: { Authorization: `Bearer ${token}` }  //Aithorization Header
-};
