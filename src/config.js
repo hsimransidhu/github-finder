7ae842af1f0d0404775dc84566ecdd9a8cb4589e
hsimransidhu <simusidhu2004@gmail.com>
2024-06-14 02:40:08 -0500

    added .env file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
  
-export const token = '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';  // GitHub API token for authentication
+ export const token = process.env.REACT_APP_GITHUB_TOKEN;
+// GitHub API token for authentication
 export const options = {
   headers: { Authorization: `Bearer ${token}` }  //Aithorization Header
 };
